2e69d2934f49f7bee1073f803aa91a6c200fc69f
nikita.oleynik <n.oleynik1995@gmail.com>
2020-07-17 22:47:57 +0300

    HTTP Request struct was created

---
+++ clientRequest.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"net/url"
+)
+
+//HTTPReq stores httpRequest fileds
+type HTTPReq struct {
+	Method     string      `json:"method"`
+	Proto      string      `json:"proto"`
+	Header     http.Header `json:"header"`
+	Body       string      `json:"body"`
+	Host       string      `json:"host"`
+	Form       url.Values  `json:"form"`
+	Trailer    http.Header `json:"trailer"`
+	RemoteAddr string      `json:"remoteAddr"`
+}
